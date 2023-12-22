4a441d286794af075ed35d62ae610142d8e96bd8
mohamedaminebz <bouzidi.amine0301@gmail.com>
2023-12-22 20:28:55 +0100

    Add full filter

--- src/main/java/tn/uma/boutiti/bouzidi/ing/projet/repository/TaskRepository.java
+++ src/main/java/tn/uma/boutiti/bouzidi/ing/projet/repository/TaskRepository.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+ 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import tn.uma.boutiti.bouzidi.ing.projet.models.Label;
@@ -27,5 +30,27 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 	 List<Task> findByLabels_NameContainingOrDescriptionContainingOrTitleContainingOrProject_NameContaining(
 	    	    String labelKeyword, String descriptionKeyword, String titleKeyword, String projectKeyword);
 
-	 
+	 @Modifying
+	    @Query(value =
+	            "SELECT * FROM Task t " +
+	                    "WHERE (:labelIds IS NULL OR EXISTS (SELECT 1 FROM task_labels l WHERE l.label_id IN :labelIds AND l.task_id = t.id)) " +
+	                    "AND (:projectId IS NULL OR t.project_id = :projectId) " +
+	                    "AND (LOWER(t.title) LIKE LOWER(CONCAT('%', :title, '%')) OR LOWER(t.description) LIKE LOWER(CONCAT('%', :description, '%'))) " +
+	                    "AND (:completed IS NULL OR t.completed = :completed) " +
+	                    "AND (:minStartDate IS NULL OR t.start_Date >= :minStartDate) " +
+	                    "AND (:maxStartDate IS NULL OR t.start_Date <= :maxStartDate) " +
+	                    "AND (:minDueDate IS NULL OR t.due_Date >= :minDueDate) " +
+	                    "AND (:maxDueDate IS NULL OR t.due_Date <= :maxDueDate)",
+	            nativeQuery = true)
+	    List<Task> findByLabels_IdInAndProject_IdAndTitleContainingOrDescriptionContaining(
+	            @Param("labelIds") List<Long> labelIds,
+	            @Param("projectId") Long projectId,
+	            @Param("title") String title,
+	            @Param("description") String description,
+	            @Param("completed") Boolean completed,
+	            @Param("minStartDate") LocalDate minStartDate,
+	            @Param("maxStartDate") LocalDate maxStartDate,
+	            @Param("minDueDate") LocalDate minDueDate,
+	            @Param("maxDueDate") LocalDate maxDueDate
+	    );
 }
