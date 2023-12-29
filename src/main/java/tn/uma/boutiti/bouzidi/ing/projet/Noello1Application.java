aa5993ec0c8b10238d8aed8f552d99da83c0acfd
mohamedaminebz <bouzidi.amine0301@gmail.com>
2023-12-29 20:12:48 +0100

    update cros add AllowedOrigins to port  3000

--- src/main/java/tn/uma/boutiti/bouzidi/ing/projet/Noello1Application.java
+++ src/main/java/tn/uma/boutiti/bouzidi/ing/projet/Noello1Application.java
@@ -46,7 +46,7 @@ public class Noello1Application implements CommandLineRunner {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:3000"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
                 "Access-Control-Request-Headers"));
